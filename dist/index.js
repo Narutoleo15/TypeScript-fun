c4312ebd4cfb54df965bf77451612d8f6843aacb
Dev-Ricky <narutoleo15@yahoo.com>
2023-01-27 02:43:54 -0600

    wahoo fixed my ts config

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let id = 1;
+const myFunc = (a, b) => { return a + b; };
+console.log(myFunc(10, 10));
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+let id : number = 1
+
+const myFunc = (a:number, b:number) => {return a + b}
+console.log(myFunc(10,10))
